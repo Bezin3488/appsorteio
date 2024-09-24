f9ec70860934a56201a05d086f83b0b2f7560a04
Bezin3488 <bernardopristupa@luteranodapaz.com.br>
2024-09-24 16:41:02 -0300

    Prova

---
+++ app/src/main/java/com/example/appsorteio/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.appsorteio
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun sortear(view: View) {
+        val rr = findViewById<TextView>(R.id.resul)
+        val num = Random.nextInt(100)
+        rr.setText("numero sorteado: $num")
+    }
+}
